109aff039e8779c72799780bf13b9b487f136deb
Doaa Oukal <doaaoukal@gmail.com>
2026-06-03 23:58:28 +0300

    Update UserController.java

--- GHADS/src/controller/UserController.java
+++ GHADS/src/controller/UserController.java
@@ -70,7 +70,7 @@ public class UserController extends BaseController {
         colUsername.setCellValueFactory(new PropertyValueFactory<>("username"));
         colEmail.setCellValueFactory(new PropertyValueFactory<>("email"));
         colRole.setCellValueFactory(new PropertyValueFactory<>("role"));
-        colOrg.setCellValueFactory(new PropertyValueFactory<>("orgName")); // ✅ Fix: عمود المنظمة كان ناقص
+        colOrg.setCellValueFactory(new PropertyValueFactory<>("orgName"));
     }
 
     private void setupCombos() {
